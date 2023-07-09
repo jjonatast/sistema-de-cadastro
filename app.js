fff0f20cc61b0a76e62f75ee2d3ac207683b9666
unknown <jjonatas@outlook.com>
2023-07-09 16:01:03 -0300

    novo log na linha 3

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log(" aplicação esta se conectando o banco de dados")
-
-console.log(" acrescentando um novo texto")
+console.log(" nova aplicção")
 
 
