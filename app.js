2ae4a6fe0be20748ed959b0a0bcb3fa92f2bd782
Jonatas Teixeira <jjonatas@outlook.com>
2023-07-09 16:07:08 -0300

    nova linha 12

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log(" aplicação esta se conectando o banco de dados")
 console.log(" nova aplicação!")
+console.log(" JONATAS TEIXEIRA")
+
 
 
